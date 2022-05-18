f349c9ef8a54e49a5a8cfb79e92e9b69de1f0e01
sammy-code98 <sammysmart98@yahoo.com>
2022-05-18 22:41:22 +0100

    feat:add modal component

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ModalBox from './Modal'
 
 export default function Docs() {
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => setOpen(true)
   return (
     <div className='docs-container'>
     <h1>docs</h1>
 
-    <button className='add-docs'>
+    <button className='add-docs' onClick={handleOpen}>
             Add a Document
         </button>
+
+        <ModalBox open={open} setOpen={setOpen}/>
    </div>  )
 }
