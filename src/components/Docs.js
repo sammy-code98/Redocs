cc8231d3d4a103ef7bf5362563f3b7d3ce54d984
sammy-code98 <sammysmart98@yahoo.com>
2022-05-18 23:13:43 +0100

    modal component

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react'
-import ModalBox from './Modal'
+import React, { useState } from "react";
+import ModalBox from "./Modal";
 
 export default function Docs() {
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => setOpen(true)
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
   return (
-    <div className='docs-container'>
-    <h1>docs</h1>
+    <div className="docs-container">
+      <h1>docs</h1>
 
-    <button className='add-docs' onClick={handleOpen}>
-            Add a Document
-        </button>
+      <button className="add-docs" onClick={handleOpen}>
+        Add a Document
+      </button>
 
-        <ModalBox open={open} setOpen={setOpen}/>
-   </div>  )
+      <ModalBox open={open} setOpen={setOpen} />
+    </div>
+  );
 }
