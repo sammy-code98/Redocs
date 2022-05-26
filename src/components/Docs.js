04fc7f6a24a8ed3d0823c78ee7a8aa5aa1271d92
sammy-code98 <sammysmart98@yahoo.com>
2022-05-26 22:43:27 +0100

    feat:display data on ui

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ModalBox from "./Modal";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
 export default function Docs({ database }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false)
+  const handleClose = () => setOpen(false);
 
   // state to hold data
   const [title, setTitle] = useState("");
+
+  // state to hold fetched data
+  const [docsData, setDocsData] = useState([]);
+
   // take the database that we got from the firebaseConfig.js and the name of the collection we want to use.
   const collectionRef = collection(database, "docsData");
 
@@ -18,12 +22,32 @@ export default function Docs({ database }) {
     })
       .then(() => {
         alert("Data added");
-        handleClose()
+        handleClose();
       })
       .catch(() => {
         alert("cannot add data");
       });
   };
+
+  const getDocs = () => {
+    onSnapshot(collectionRef, (data) => {
+      setDocsData(
+        data.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        })
+      );
+    });
+  };
+  // to prevent the  concurrent rendering react v18 use the useRef
+
+  const isMounted = useRef();
+  useEffect(() => {
+    if (isMounted.current) {
+      return;
+    }
+    isMounted.current = true;
+    getDocs();
+  }, []);
   return (
     <div className="docs-container">
       <h1>my docs</h1>
@@ -39,6 +63,16 @@ export default function Docs({ database }) {
         setTitle={setTitle}
         addDocs={addDocs}
       />
+
+      <div className="grid-main">
+        {docsData.map((doc) => {
+          return (
+            <div key={doc.id} className="grid-child">
+              <p>{doc.title}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
