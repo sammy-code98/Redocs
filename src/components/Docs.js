f901869c63ef7fc39ea0dc68aeb3bd810a0105c3
sammy-code98 <sammysmart98@yahoo.com>
2022-06-02 21:07:40 +0100

    refactor

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -90,7 +90,7 @@ export default function Docs({ database }) {
                 <DocCard
                   title={doc.title}
                   // dangerouslySetInnerHTML={{ __html: doc.docsDesc }}
-                  docsDesc={{__html: doc.docsDesc }}
+                  docsDesc={doc.docsDesc}
                   // createdAt={doc.createdAt.toDate().getTime()}
                 />
               </div>
