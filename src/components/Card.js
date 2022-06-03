06a2eb3db09a6d33ef32feafd7e8c4231d77e04d
sammy-code98 <sammysmart98@yahoo.com>
2022-06-03 22:26:42 +0100

    card update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
@@ -10,20 +11,22 @@ import doc from "../docs.png";
 
 export default function DocCard({ title, docsDesc, createdAt }) {
   return (
-    <Card sx={{ maxWidth: 250, cursor: "pointer" }}>
-      <CardMedia component="img" height="194" image={doc} alt="docs image" />
-      <Typography
-        sx={{
-          textAlign: "left",
-          mx: "8px",
-          mt: "4px",
-          textTransform: "capitalize",
-          fontWeight: "medium",
-        }}
-        variant="body2"
-      >
-        {title}
-      </Typography>
+    <Card sx={{ maxWidth: 250 }}>
+      <CardContent sx={{ cursor: "pointer" }}>
+        <CardMedia component="img" height="194" image={doc} alt="docs image" />
+        <Typography
+          sx={{
+            textAlign: "left",
+            mx: "8px",
+            mt: "4px",
+            textTransform: "capitalize",
+            fontWeight: "medium",
+          }}
+          variant="body2"
+        >
+          {title}
+        </Typography>
+      </CardContent>
 
       <CardActions
         disableSpacing
