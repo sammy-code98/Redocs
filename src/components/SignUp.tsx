0bd37e96d8c86a4119c4a7981856fda2107a6713
sammy-code98 <sammysmart98@yahoo.com>
2023-05-29 16:45:56 +0100

    add sign up page

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+const signup = require("../assets/btn.png")
+
+
+export default function SignUp() {
+    return (
+        <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            paddingTop='6rem'
+
+
+        >
+            <Typography variant="h2" gutterBottom component="div">Welocome to Redocs</Typography>
+
+            <Typography variant="h4" gutterBottom component="div">Redocs is a simple Google Docs clone </Typography>
+
+
+            <Typography variant="h6" gutterBottom component="div">To get started, sign up with your Google Account</Typography>
+
+            <Box marginTop='6rem'>
+                <img src={signup} alt='signup' className='signUp-img' />
+            </Box>
+
+        </Box>
+    )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ import Docs from "components/Docs";
 import EditDocs from "components/EditDocs";
 import ModalBox from "components/Modal";
 import MenuBtn from "components/MenuBtn";
-export { DocCard, Docs, EditDocs, ModalBox, MenuBtn };
+import SignUp from "./SignUp";
+
+export { DocCard, Docs, EditDocs, ModalBox, MenuBtn, SignUp };
